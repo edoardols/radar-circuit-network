180330300ddfdd5bbe00eeba4854c577097762ca
edoardols <elascala@outlook.it>
2023-02-08 09:03:26 +0100

    initial commit

---
+++ control.lua
@@ -0,0 +1,12 @@
+-- ciclare tra le entità radar
+
+-- scegliere solo quelle che sono collegate al circuit network
+
+-- vedere se nell'aria di mappa rivelata, NON QUELLA CHE SCANSIONANO, se ci sono basi nemiche (spawner spitter and biters) oppure worms
+
+-- se ci sono scrive il numero che legge in uscita
+
+
+-- ma va scelta un'uscita e li porta in uscita CFR combinatore
+
+-- oppure aggiungere biter come segnale ?
